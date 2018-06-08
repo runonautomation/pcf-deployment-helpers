91a1feab027b25c9553aed808503ce8c917af929
Vladimir <runonautomation@gmail.com>
2018-06-08 21:52:41 +0300

    Update 01_create_service_account.sh

--- 01_create_service_account.sh
+++ 01_create_service_account.sh
@@ -1,5 +1,7 @@
 echo # RUN THIS FROM PRIVATE CLOUD SHELL
 PROJECT_ID=$GCP_PROJECT_ID
+#PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+
 gcloud iam service-accounts create deploy --display-name "deploy"
 gcloud iam service-accounts keys create deploy.key.json --iam-account deploy@$PROJECT_ID.iam.gserviceaccount.com
 gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:deploy@$PROJECT_ID.iam.gserviceaccount.com" --role 'roles/owner'
