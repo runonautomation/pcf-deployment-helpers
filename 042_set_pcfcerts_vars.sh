402ce5ad027dd930991af6d6181e085956017863
Volodymyr Davydenko <volodymyr.davydenko@avid.com>
2018-08-24 19:19:11 +0300

    Update permissions

--- 042_set_pcfcerts_vars.sh
+++ 042_set_pcfcerts_vars.sh
@@ -1,10 +1,11 @@
+set -e
 export DOMAIN=${DOMAIN:-example.com}
 export TERRAFORM_SOLUTION_PATH=${TERRAFORM_SOLUTION_PATH:-./pcfcerts}
 export SSLCA=$(cat gen/root-ca/certs/ca.crt)
 export SSLCAK=$(cat gen/root-ca/private/ca.key)
 export PROJECT_ID=$(cat deploy.key.json  | grep project_id | awk -F\" '{print $4}')
 export REGION=us-central1
-export SK=$(cat deploy.key.json)
-envsubst < "tfvars" > "$TERRAFORM_SOLUTION_PATH/terraform.tfvars"
+export SK=$(cat ~/deploy.key.json)
 
+envsubst < "tfvars" > "$TERRAFORM_SOLUTION_PATH/terraform.tfvars"
 cat makecerts/terraform.tfvars
