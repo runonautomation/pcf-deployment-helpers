2201c99eeb53fa4fbec67ae76aac508632cd10ef
Volodymyr Davydenko <volodymyr.davydenko@avid.com>
2018-08-24 19:25:31 +0300

    Update

--- 041_set_terraforming_vars.sh
+++ 041_set_terraforming_vars.sh
@@ -1,10 +1,14 @@
 set -e
 export DOMAIN=${DOMAIN:-example.com}
 export TERRAFORM_SOLUTION_PATH=${TERRAFORM_SOLUTION_PATH:-./terraforming-gcp}
+export KEY_LOCATION=${KEY_LOCATION:-~/deploy.key.json}
+
 export SSLCA=$(cat gen/root-ca/certs/ca.crt)
 export SSLCAK=$(cat gen/root-ca/private/ca.key)
-export PROJECT_ID=$(cat deploy.key.json  | grep project_id | awk -F\" '{print $4}')
+
+export PROJECT_ID=$(cat $KEY_LOCATION  | grep project_id | awk -F\" '{print $4}')
 export REGION=us-central1
-export SK=$(cat ~/deploy.key.json)
+export SK=$(cat $KEY_LOCATION)
+
 envsubst < "tfvars" > "$TERRAFORM_SOLUTION_PATH/terraform.tfvars"
 cat $TERRAFORM_SOLUTION_PATH/terraform.tfvars
